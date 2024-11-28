9561777579356dd264bceaad27a982cbdf04767d
Jakub Knap <jknap40@gmail.com>
2024-11-28 11:09:29 +0100

    initial commit

---
+++ src/main/java/pl/scoottrack/ScootTrackApplication.java
@@ -0,0 +1,12 @@
+package pl.scoottrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScootTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScootTrackApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/scoottrack/ScootTrackApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.scoottrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScootTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
